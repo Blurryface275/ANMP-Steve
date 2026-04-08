507ff17fb75e4016cf12894a60d087d7d708734e
Stephen <stephen.sugianto234@gmail.com>
2026-04-08 08:37:26 +0700

    Implementasi error handling pada request Volley di DetailViewModel

--- app/src/main/java/com/example/studentproject/view/DetailViewModel.kt
+++ app/src/main/java/com/example/studentproject/view/DetailViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.studentproject.view
 
 import android.R
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.android.volley.RequestQueue
@@ -10,13 +11,17 @@ import com.android.volley.toolbox.Volley
 class DetailViewModel: ViewModel() {
 
     val studentLD = MutableLiveData<Student>()
+    val errorLD = MutableLiveData<Boolean>(0)
     val TAG:String = "Volley Tag"
     var queue: RequestQueue?= null
     fun fetch(student : Student) {
         queue = Volley.newRequestQueue(getApplication())
         val url = "https://www.jsonkeeper.com/b/LLMW"
 
-        val stringRequest(Request.Method.GET, url, {},{})
+        val stringRequest(Request.Method.GET, url, {},{
+            Log.d("volley_status", it.message.toString())
+            errorLD.value = true
+        })
         stringRequest.tag = TAG
         queue.add(stringRequest)
 
