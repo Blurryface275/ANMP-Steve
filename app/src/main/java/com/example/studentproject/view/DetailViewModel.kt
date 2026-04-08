12e4669daf6d0b3033ccc8df03deb3aaafe01c6d
Stephen <stephen.sugianto234@gmail.com>
2026-04-08 08:18:40 +0700

    Tambahan detail view model

--- app/src/main/java/com/example/studentproject/view/DetailViewModel.kt
+++ app/src/main/java/com/example/studentproject/view/DetailViewModel.kt
@@ -1,17 +1,26 @@
 package com.example.studentproject.view
 
+import android.R
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.RequestQueue
 import com.example.studentproject.model.Student
+import com.android.volley.toolbox.Volley
 
 class DetailViewModel: ViewModel() {
 
     val studentLD = MutableLiveData<Student>()
+    val TAG:String = "Volley Tag"
+    var queue: RequestQueue?= null
     fun fetch(student : Student) {
-
+        queue = Volley.newRequestQueue(getApplication())
+        val url = "https://www.jsonkeeper.com/b/LLMW"
         studentLD.value = student
     }
 
+    private fun getApplication() {
+        TODO("Not yet implemented")
+    }
 
 
 }
